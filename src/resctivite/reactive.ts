45c80e83b134f6adf021a7bbacc55cfb30432d78
zlb <1052519137@qq.com>
2022-10-15 00:06:35 +0800

    uoate

---
+++ src/resctivite/reactive.ts
@@ -0,0 +1,37 @@
+import { mutableHandlers, readonlyHandlers, shallowReadonlyHandlers } from "./baseHandle";
+
+export const enum ReactiveFlags {
+    IS_REACTIVE = "__v_isReactive",
+    IS_READONLY = "__v_isReadonly",
+    RAW = "__v_raw",
+}
+
+
+export function reactive (raw: object) {
+    return createReactiveObject(raw, mutableHandlers)
+}
+
+export function readonly (raw: object) {
+    return createReactiveObject(raw, readonlyHandlers)
+}
+
+export function shallowReadonly (raw: object) {
+    return createReactiveObject(raw, shallowReadonlyHandlers)
+}
+
+function createReactiveObject (raw, handle) {
+    return new Proxy(raw, handle)
+}
+
+export function isProxy (value: any) {
+    return isReactive(value) || isReadonly(value)
+}
+
+
+export function isReadonly (value: any) {
+    return !!value[ReactiveFlags.IS_READONLY]
+}
+
+export function isReactive (value: any) {
+    return !!value[ReactiveFlags.IS_REACTIVE]
+}
